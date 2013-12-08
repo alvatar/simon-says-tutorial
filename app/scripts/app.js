96172ca0f995bf10fdb27792c185cc160be43be8
Álvaro <alvcastro@yahoo.es>
2013-12-08 08:57:15 -0500

    buttons and basic jquery interaction

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,25 @@
 /*global define */
 define([], function () {
     'use strict';
+    
+    $(document).ready(function() {
+        $(".button").click( function(){
+             var thisId = $(this).attr('id')
+             if(thisId === "left-top") {
+                 alert("Yes!")
+             } else if ( thisId === "right-top") {
+
+             } else if ( thisId === "left-bottom") {
+
+             } else if ( thisId === "right-bottom") {
+
+             } else {
+                  console.log("Wrong button ID detected")
+             }
+        })
+    })
+    return "<============== OK";
 
     return '\'Allo \'Allo!';
 });
+
